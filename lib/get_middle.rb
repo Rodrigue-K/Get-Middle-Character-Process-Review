3a429f1e9f13267bb37df0a6a836f19d67dab512
Rodrigue <Rodriguekayembe@gmail.com>
2019-04-05 15:34:21 +0100

    rubocop

--- lib/get_middle.rb
+++ lib/get_middle.rb
@@ -1,6 +1,6 @@
 def get_middle(word)
   word_length = word.length
-  word_halved = word_length/2
+  word_halved = word_length / 2
   if word_length.even?
     word[word_halved - 1..word_halved]
   else
